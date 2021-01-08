05138fd02ce92bc384e5efe20374f2f95808f230
224088 <224088@edu.p.lodz.pl>
2021-01-09 00:40:01 +0200

    dodalam logowanie googlem jutro sprawdze czy dziala na zwyklych telefonach

--- FINALPROJECT/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FINALPROJECT/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -10,31 +10,50 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.GoogleAuthProvider;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
+    private GoogleSignInClient mSignInClient;
     private CallbackManager mCallbackMenager;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
-    private LoginButton login;
+    private LoginButton FBlogin;
     private AccessTokenTracker accessTokenTracker;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+    private EditText UserEmail, UserPassword;
     private static final String TAG = "FacebookAuthentiblabla";
+    private SignInButton googleSignInButton;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +64,30 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
         FacebookSdk.sdkInitialize(getApplicationContext());
 
-        login = findViewById(R.id.login_button);
-        login.setReadPermissions("email", "public_profile");
+        UserEmail = (EditText) findViewById(R.id.login_EditText);
+        UserPassword = (EditText) findViewById(R.id.password_EditText);
+        googleSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        LoginButton = (Button) findViewById(R.id.login_button);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowingUserToLogin();
+            }
+        });
+
+
+//-----------------FBLogin------------------------------------
+        FBlogin = findViewById(R.id.fb_login_button);
+        FBlogin.setReadPermissions("email", "public_profile");
         mCallbackMenager = CallbackManager.Factory.create();
-        login.registerCallback(mCallbackMenager, new FacebookCallback<LoginResult>() {
+        FBlogin.registerCallback(mCallbackMenager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "onSuccess" + loginResult);
@@ -65,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onError" + error);
             }
         });
+
 
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -86,6 +126,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mSignInClient = GoogleSignIn.getClient(this, gso);
 
     }
 
@@ -109,11 +157,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
+    /*@Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         mCallbackMenager.onActivityResult(requestCode,resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-    }
+    }*/
 
     private void updateUI(FirebaseUser user) {
         if(user != null) {
@@ -122,10 +170,78 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+    private void signIn()
+    {
+        Intent signInIntent = mSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN)
+        {
+            loadingBar.setTitle("google Sign In");
+            loadingBar.setMessage("Please wait, while we are allowing you to login using your Google Account...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess())
+            {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                Toast.makeText(this, "Please wait, while we are getting your auth result...", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Can't get Auth result.", Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }
+    }
+
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mFirebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Log.d(TAG, "signInWithCredential:success");
+                            //SendUserToMainActivity();
+                            loadingBar.dismiss();
+                        }
+                        else
+                        {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            String message = task.getException().toString();
+                            //SendUserToLoginActivity();
+                            Toast.makeText(MainActivity.this, "Not Authenticated : " + message, Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
-        mFirebaseAuth.addAuthStateListener(authStateListener);
+        FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 
     @Override
@@ -135,4 +251,51 @@ public class MainActivity extends AppCompatActivity {
             mFirebaseAuth.removeAuthStateListener(authStateListener);
         }
     }
-}
+
+
+    private void AllowingUserToLogin()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please write your email...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login");
+            loadingBar.setMessage("Please wait, while we are allowing you to login into your Account...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+
+            mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                               // SendUserToMainActivity();
+
+                                Toast.makeText(MainActivity.this, "you are Logged In successfully.", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(MainActivity.this, "Error occured: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+
+    }

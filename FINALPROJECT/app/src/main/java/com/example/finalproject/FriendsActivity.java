f4c424b64c1d88030251e6df0780cd1861e42b83
224088 <224088@edu.p.lodz.pl>
2021-01-25 21:49:00 +0200

    Dodawanie przyjaciol zrobioneee

--- FINALPROJECT/app/src/main/java/com/example/finalproject/FriendsActivity.java
+++ FINALPROJECT/app/src/main/java/com/example/finalproject/FriendsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PersonProfileActivity extends AppCompatActivity {
+public class FriendsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person_profile);
+        setContentView(R.layout.activity_friends);
     }
 }
